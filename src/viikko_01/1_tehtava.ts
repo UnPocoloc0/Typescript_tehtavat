7dfb269e6e4589a85dfdc8448e8694176eccee39
Simo Loikkanen <simoloikkanen@Simo-MacBook-Pro.local>
2026-09-04 16:55:41 +0300

    Valitallennus

--- src/viikko_01/1_tehtava.ts
+++ src/viikko_01/1_tehtava.ts
@@ -3,3 +3,13 @@
 */
 
 type UserStatus = "active" | "inactive" | "pending";
+
+/*
+2. Define a tuple type Coordinate containing exactly two numbers: [latitude: number, longitude: number].
+*/
+
+type Coordinate = [latitude: number, longitude: number];
+
+/*
+3. Define a type alias UserProfile with properties: - id: readonly number - user: string - email: string - status: UserStatus - location: Coordinate - bio: optional string.
+*/
