5624bea17082421328ec8d3d036dda931f2d885d
Simo Loikkanen <sloikkanen@gmail.com>
2026-09-04 15:16:15 +0300

    Lisää testiprofiili

--- src/viikko_01/1_tehtava.ts
+++ src/viikko_01/1_tehtava.ts
@@ -1,15 +1,41 @@
 /*
-1. Define a string literal union type UserStatus that accepts only "active", "inactive", or "pending". 
+1. Define a string literal union type UserStatus that accepts only "active",
+   "inactive", or "pending". 
 */
 
 type UserStatus = "active" | "inactive" | "pending";
 
 /*
-2. Define a tuple type Coordinate containing exactly two numbers: [latitude: number, longitude: number].
+2. Define a tuple type Coordinate containing exactly two numbers: [latitude:
+   number, longitude: number].
 */
 
 type Coordinate = [latitude: number, longitude: number];
 
 /*
-3. Define a type alias UserProfile with properties: - id: readonly number - user: string - email: string - status: UserStatus - location: Coordinate - bio: optional string.
+3. Define a type alias UserProfile with properties: - id: readonly number -
+   user: string - email: string - status: UserStatus - location: Coordinate -
+   bio: optional string.
 */
+
+
+type UserProfile = {
+
+  readonly id: number;
+  user: string;
+  email: string;
+  status: UserStatus;
+  location: Coordinate;
+  bio?: string;
+
+};
+
+const user: UserProfile = {
+  id: 1,
+  user: "simo",
+  email: "simo@gmail.com",
+  status: "active",
+  location: [33.33, 66.66]
+};
+
+console.log(user);
